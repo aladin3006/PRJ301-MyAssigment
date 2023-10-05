1849a8d1b43a8dbb33651782b0852c728d879300
aladin3006 <nguyenthidaodb123@gmail.com>
2023-10-06 00:46:00 +0700

    Test a basic login html file
    use chat gpt to create a basic display login

--- MyProject/src/java/controller/NewServlet.java
+++ MyProject/src/java/controller/NewServlet.java
@@ -19,24 +19,12 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 @WebServlet(name="NewServlet", urlPatterns={"/NewServlet"})
 public class NewServlet extends HttpServlet {
-   
-    
-    
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
-    } 
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.html").forward(req, resp);
+    }
+    
     /** 
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
